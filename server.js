df7d6cac78a80fd220d035bdbb6b87db329e5f81
spiceyourcode <franklineomari@gmail.com>
2025-10-23 16:37:46 +0300

    feat(db): add MongoDB connection and update imports
    - Add new dbConnection.js for MongoDB setup
    - Update imports to include .js extensions for ES modules
    - Add mongoose dependency and set package type to module
    - Rename errorHandle to errorHandler for consistency
    - Update server.js to initialize database connection

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
-import errorHandle from "./middleware/errorHandler.js"
+import errorHandler from "./middleware/errorHandler.js"
+import connectDb from "./config/dbConnection.js";
 
+// initiating the database instance 
+connectDb()
 dotenv.config();
+// starting the express server 
 const app = express();
 // loading the environment variables
 const port = process.env.PORT;
@@ -13,9 +17,9 @@ const port = process.env.PORT;
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/contacts", contactRoutes);
-app.use(errorHandle);
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Contact app listening at http://localhost:${port}`);
 })
 
